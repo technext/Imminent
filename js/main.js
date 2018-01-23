5ce8d2566b20d54477a7a052bf4759a36ba0d32b
Shahriar Rahman <shahriar.technext@gmail.com>
2018-01-23 10:11:39 +0600

    timer

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 13, 2019 18:03:26",
+	date: "December 14, 2019 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
