a4291f81e37f31e4796c302cd011cc1ec5de5213
kanijfatemalaizu <kanij.fatema.laizu@gmail.com>
2020-04-11 11:17:20 +0600

    change count down date

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 14, 2019 18:03:26",
+	date: "Mar 25 2021",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
